695a10992de36e66bf41f6ac2293e13af8782572
HypertextPP <mail@bradleyw.me>
2018-09-14 11:36:54 +0100

    UPDATED: Tests to be more real-world.

--- tests/settings.test.ts
+++ tests/settings.test.ts
@@ -2,29 +2,20 @@ import assert from "assert";
 import path from "path";
 
 import { Core, Settings } from "kratecms";
-const settings = new Settings(
-  path.join(__dirname, "..", "src", "KrateCMS", "settings")
-);
+const settings = new Settings(path.join(__dirname, "settings"));
 
 describe("Settings", () => {
-  describe("#get('currentTheme')", () => {
-    it("should equal 'Krate2018'", () => {
-      Core.on("loaded", () => {
-        assert.equal(Settings.get("currentTheme"), "Krate2018");
-      });
-    });
-  });
   describe("#set('test', 'just testing')", () => {
     it("should equal 'just testing'", () => {
       Core.on("loaded", () => {
-        assert.equal(Settings.set("test", "just testing"), "just testing");
+        assert.equal(settings.set("test", "just testing"), "just testing");
       });
     });
   });
   describe("#get('test')", () => {
     it("should equal 'just testing'", () => {
       Core.on("loaded", () => {
-        assert.equal(Settings.get("test"), "just testing");
+        assert.equal(settings.get("test"), "just testing");
       });
     });
   });
